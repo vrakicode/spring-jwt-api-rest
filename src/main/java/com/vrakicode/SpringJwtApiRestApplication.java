19a47b56d76e83838e8d379d7b5a4ebaaaf1fb59
Victor <vrakimail@gmail.com>
2023-11-16 19:16:59 -0500

    primera beta

---
+++ src/main/java/com/vrakicode/SpringJwtApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.vrakicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtApiRestApplication.class, args);
+	}
+
+}
